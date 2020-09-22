107f50fc6625f6fee634bb344b4ca078273dc53c
meHimanshu <himanshuu5295@gmail.com>
2020-09-22 14:55:46 +0530

    prod fix

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use('/api',router);
 // catch 404 and forward to error handler
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '..client/build')));
     
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
